f8d558d918ddf9ad7601bf4dfde100f317429770
Sebastien Binet <binet@cern.ch>
2014-10-30 09:52:16 +0100

    deps: make concurrent

--- cmd_deps.go
+++ cmd_deps.go
@@ -31,14 +31,29 @@ func gohepRunCmdDeps(cmd *commander.Command, args []string) error {
 		pkgs = args
 	}
 
+	depch := make(chan []string)
+	errch := make(chan error)
+
 	set := make(map[string]struct{})
 	for _, pkg := range pkgs {
-		deps, err := godeps(pkg)
-		if err != nil {
+		go func(pkg string) {
+			deps, err := godeps(pkg)
+			if err != nil {
+				errch <- err
+				return
+			}
+			depch <- deps
+		}(pkg)
+	}
+
+	for _ = range pkgs {
+		select {
+		case err := <-errch:
 			return err
-		}
-		for _, dep := range deps {
-			set[dep] = struct{}{}
+		case deps := <-depch:
+			for _, dep := range deps {
+				set[dep] = struct{}{}
+			}
 		}
 	}
 
